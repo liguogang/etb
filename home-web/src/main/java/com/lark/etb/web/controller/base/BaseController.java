d936bc56a91a3a3f9af18bf74d1f0089a24fb410
guogang.lgg <guogang.lgg@alibaba-inc.com>
2016-04-04 01:32:41 +0800

    controller

---
+++ home-web/src/main/java/com/lark/etb/web/controller/base/BaseController.java
@@ -0,0 +1,135 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+package com.lark.etb.web.controller.base;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.jasper.security.SecurityUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.ui.Model;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * 控制器 基础类，放一些基础工具 
+ * @author liguogang
+ * @version $Id: BaseController.java, v 0.1 2016年4月4日 上午12:42:55 liguogang Exp $
+ */
+public class BaseController {
+    private static final Logger logger = LogManager.getLogger(BaseController.class);
+    
+    private static final String JSONP_CALLBACK = "callback";
+    
+    private static final String RESPONSE_RESULT = "result";
+    
+    private static final String JSON_SUFFIX = ".json";
+    
+    private static final String DO_SUFFIX = ".do";
+
+    /***
+     * 
+     * 
+     * @param request
+     * @param response
+     * @param model
+     * @param data
+     */
+    protected void autoResponse(HttpServletRequest request, HttpServletResponse response, Model model, Object data) {
+        try {
+            if (null == model) {
+                this.responseJson(request,response,data);
+                return;
+            }
+            
+            if (null == request) {
+                model.addAttribute(RESPONSE_RESULT, data);
+                return;
+            }
+            
+            if (isResponseJSON(request)) {
+                this.responseJson(request, response, data);
+            }else{
+                model.addAttribute(RESPONSE_RESULT, data);
+            }
+        } catch (Exception e) {
+            logger.error("autoResponse,N",e);
+        }
+    }
+    
+    /***
+     * 响应json 或 jsonp
+     * 
+     * @param request
+     * @param response
+     * @param data
+     * @throws IOException
+     */
+    protected void responseJson(HttpServletRequest request,HttpServletResponse response, Object data) throws IOException{
+        String callback = this.callback(request);
+        PrintWriter writer = response.getWriter();
+        writer.print("\r\t");
+        String jsonStr = this.toJson(data);
+        
+        if (null == callback) {
+            writer.print(jsonStr);
+        }else{
+            //SecurityUtil.filter
+            writer.print(callback);
+            writer.print("(");
+            writer.print(jsonStr);
+            writer.print(")");
+        }
+        
+        writer.flush();
+        writer.close();
+    }
+    
+    private String toJson(Object data){
+        if (null == data) {
+            return "";
+        }
+        
+        if (data instanceof String) {
+            return (String)data;
+        }else {
+            return JSONObject.toJSONString(data);
+        }
+    }
+    
+    /***
+     * response json的判断
+     * 
+     * @param request
+     * @return
+     */
+    private boolean isResponseJSON(HttpServletRequest request){
+        if (null == request) {
+            return false;
+        }
+        
+        String requestUrl = request.getRequestURI();
+        if(requestUrl.endsWith(JSON_SUFFIX)){
+            return true;
+        } 
+        
+        if(requestUrl.endsWith(DO_SUFFIX)){
+            return true;
+        }
+        
+        return false;
+    }
+
+    private String callback(HttpServletRequest request) {
+        if (null == request) {
+            return null;
+        }
+        return request.getParameter(JSONP_CALLBACK);
+    }
+}
--- home-web/src/main/java/com/lark/etb/web/controller/buy/OrderController.java
+++ home-web/src/main/java/com/lark/etb/web/controller/buy/OrderController.java
@@ -4,12 +4,22 @@
  */
 package com.lark.etb.web.controller.buy;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.lark.etb.web.controller.base.BaseController;
 import com.lark.ets.buy.client.OrderServiceClient;
 
 /**
@@ -18,23 +28,35 @@ import com.lark.ets.buy.client.OrderServiceClient;
  * @version $Id: OrderController.java, v 0.1 2016年3月19日 下午4:05:43 liguogang Exp $
  */
 @Controller
-public class OrderController implements InitializingBean{
+@RequestMapping("order")
+public class OrderController extends BaseController implements InitializingBean {
     private final static Logger logger = LogManager.getLogger(OrderController.class);
-    
+
     @Value("${env}")
-    private String env;
-    
-    private OrderServiceClient orderService;
-    
-    public void test(){
+    private String              env;
+
+    private OrderServiceClient  orderService;
+
+    @RequestMapping("query/{id}.*")
+    public String queryOrder(HttpServletRequest request,HttpServletResponse response,Model model,@PathVariable String id) {
+        logger.info("orderController,test,enter,id=[" + id + "]");
+        Map<String, String> data = new HashMap<String, String>();
+        data.put("id", id);
+        data.put("env", env);
+        this.autoResponse(request, response, model, data);
+        return "buy/order";
+    }
+
+    @RequestMapping("list")
+    public void list() {
         logger.info("orderController,test,enter");
     }
-    
-    public void afterPropertiesSet() throws Exception{
-        logger.debug("init,debug OrderController,env="+env);
-        logger.info("init,info OrderController,env="+env);
-        logger.warn("init,warn OrderController,env="+env);
-        logger.error("init,error OrderController,env="+env);
+
+    public void afterPropertiesSet() throws Exception {
+        logger.debug("init,debug OrderController,env=" + env);
+        logger.info("init,info OrderController,env=" + env);
+        logger.warn("init,warn OrderController,env=" + env);
+        logger.error("init,error OrderController,env=" + env);
     }
 
     public OrderServiceClient getOrderService() {
